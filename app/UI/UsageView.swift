c5c5c2c6e237f0af8fe1342ca307015a94177568
Miguel Bandeira <miguel@goncalvesbandeira.com>
2026-01-26 17:11:28 +0000

    Fix: Remove quotes from donate button text

--- app/UI/UsageView.swift
+++ app/UI/UsageView.swift
@@ -176,7 +176,7 @@ struct MultiProviderUsageView: View {
                 NSWorkspace.shared.open(URL(string: "https://donate.stripe.com/3cIcN5b5H7Q8ay8bIDfIs02")!)
             }) {
                 HStack(spacing: 4) {
-                    Text("Buy \"the original\" Dev a Coffee")
+                    Text("Buy the original Dev a Coffee")
                 }
             }
             .buttonStyle(.borderless)
